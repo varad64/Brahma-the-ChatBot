270509ec710f54f0fda899fa6efc6ae78ff57635
unknown <varad23711@gmail.com>
2020-05-29 03:35:38 +0530

    initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+blank_spot = "Belgaum"
+response_a = "The average temperature this weekend in {} will be 28 degrees. Bring your sunglasses!"
+response_b = "Forget about your umbrella; there is no rain forecasted in {} this weekend."
+response_c = "Belgaum is beautiful and close to the Western Ghats!"
+response_d = "Belgaum is a city located in the Indian state of Karnatka. It is also known as the second capital of Karnataka"
+response_e = "KLS Gogte Institute of Technology is an Indian engineering college in Belgaum, Karnataka, which is affiliated autonomous under Visvesvaraya Technological University."
+response_f = "KLS Gogte Institute of Technology was founded in the year 1979"
+response_g = "Hello"
+response_h = "Hey there!"
+response_i = "The vision of KLS Gogte Institute of Technology is that Gogte Institute of Technology shall stand out as an Institution Of Excellence in Technical Education and in training individuals for outstanding calibre, character coupled with creativity and entrepreneurial skills."
+response_j = "The mission of KLS Gogte Institute of Technology is to train the students to become Quality Engineers with High Standards of Professionalism and Ethics who have Positive Attitude, a Perfect blend of Techno-Managerial Skills  and Problem-solving ability with an analytical and innovative mindset."
+
+responses= [response_a, response_b, response_c, response_d, response_e, response_f, response_g, response_h, response_i, response_j]
+  
